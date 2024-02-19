fc87cf8e72f6b2516a943554c948a5ea5a21f4e6
Gage McGurie <72943845+Gage-McGuire@users.noreply.github.com>
2024-02-18 16:12:46 -0800

    Data processing
    restarting from scratch, code now transforms data into tensors. prompts user for input and displays img. next step is to load data and visualize the imgs.

--- EmotionDetection.py
+++ EmotionDetection.py
@@ -1,78 +1,25 @@
-#imports 
 import torch
-import torch.nn as nn
 import torchvision
-import torchvision.transforms as transforms
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
 import matplotlib.pyplot as plt
-import numpy as np
 
-batch_size = 64
-num_classes = 10
-learning_rate = 0.001
-num_epochs = 20
+data_loc = "./data/train"
+test_data_loc = "./data/test"
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+dataset = ImageFolder(data_loc, transform=transforms.Compose([
+    transforms.Resize((200,200)),transforms.ToTensor()
+    ]))
 
-transform_data = transforms.Compose([transforms.Resize((48,48)), 
-                                     transforms.ToTensor(),
-                                     transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], 
-                                                          std=[0.2023, 0.1994, 0.2010])])
+test_dataset = ImageFolder(test_data_loc, transform=transforms.Compose([
+    transforms.Resize((200,200)),transforms.ToTensor()
+    ]))
 
-training_data_loader = torch.utils.data.DataLoader(dataset = './train',
-                                                   batch_size = batch_size,
-                                                   shuffle = True)
-testing_data_loader = torch.utils.data.DataLoader(dataset = './test',
-                                                  batch_size = batch_size,
-                                                  shuffle = True)
+def display_img(img, label):
+    print(f"Label: {dataset.classes[label]}")
+    plt.imshow(img.permute(1,2,0))
+    plt.show()
 
+input = int(input("enter num: "))
 
-class ConvNeuralNet(nn.Module):
-    def __init__(self, num_classes):
-        super(ConvNeuralNet, self).__init__()
-        self.conv_layer1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
-        self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
-        self.max_pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.conv_layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
-        self.conv_layer4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3)
-        self.max_pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.fullyConnected1 = nn.Linear(1600, 128)
-        self.relu1 = nn.ReLU()
-        self.fullyConnected2 = nn.Linear(128, num_classes)
-
-    def forward(self, x):
-        out = self.conv_layer1(x)
-        out = self.conv_layer2(out)
-        out = self.max_pool1(out)
-
-        out = self.conv_layer3(out)
-        out = self.conv_layer4(out)
-        out = self.max_pool2(out)
-
-        out = out.reshape(out.size(0), -1)
-
-        out = self.fullyConnected1(out)
-        out = self.relu1(out)
-        out = self.fullyConnected2(out)
-
-        return out
-
-model = ConvNeuralNet(num_classes)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=0.005, momentum=0.9)
-total_step = len(training_data_loader)
-
-for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(training_data_loader):
-        images = images.to(device)
-        labels = labels.to(device)
-
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
-    
+display_img(*dataset[input])
